e36cf9aa6adcd3f7972c300831c49bab71581535
p1r0 <p1r0@insomnia247.nl>
2015-01-31 11:45:22 -0600

    agregamos comentarios, eliminamos impresiones inecesarias y damos formato mas legible en codigo

--- xls2sql.py
+++ xls2sql.py
@@ -16,33 +16,39 @@ if __name__ == "__main__":
         OuFile = open("first", 'w')
 #Abrmos la hoja de calculo
         book = xlrd.open_workbook(sys.argv[1])
-        print "El numero de hojas es:", book.nsheets
-        print "nombre(s) de las hojas:", book.sheet_names()
+        #print "El numero de hojas es:", book.nsheets
+        #print "nombre(s) de las hojas:", book.sheet_names()
 #Tomamos la primer Hoja
         sh = book.sheet_by_index(0)
 #Imprimimos  nombre de hoja, filas y columnas
-        print sh.name, sh.nrows, sh.ncols
+        print sh.name, sh.nrows, sh.ncols  
         for i in range(1,sh.nrows):
-            print "la fila "+str(i)+" tiene:"+sh.cell_value(rowx=i, colx=0),
+        #imprime de linea procesado
+            print "Procesando fila:"+str(i)
+
+        #manipulacion con formato de fecha xlrd y separando en variables la tupla para fecha
             Fecha = xlrd.xldate.xldate_as_tuple(sh.cell_value(rowx=i,colx=1),0)
             Anio, Mes, Dia = Fecha[0], "%02d"%Fecha[1], "%02d"%Fecha[2]
+            
+        #Agregamos comando sql y nombre de tabla al inicio
             if i == 1:
                 OuFile.writelines("Insert Into medi"+str(Anio)+str(Mes)
                         +"\n(eq_id,med_fecha,med_kwh) Values\n") 
-            print str(Dia)+"-"+str(Mes)+"-"+str(Anio),
+        #manipulacion con formato de fecha xlrd y separado en variable la tupla para hora
             Hora = xlrd.xldate.xldate_as_tuple(sh.cell_value(rowx=i,colx=2),0) 
             Horas, Minutos, Segundos =  "%02d"%Hora[3], "%02d"%Hora[4], "%02d"%Hora[5]
-            print str(Horas)+":"+str(Minutos)+":"+str(Segundos),
-            print sh.cell_value(rowx=i,colx=3)
+        #agregamos todas las celdas como filas en sql excepto la ultima
             if i < (sh.nrows-1):
                 OuFile.writelines("('"+str(sh.cell_value(rowx=i,colx=0))+"', '"+str(Anio)+"-"+str(Mes)
                         +"-"+str(Dia)+" "+str(Horas)+":"+str(Minutos)+":"
                         +str(Segundos)+"', '"+str(sh.cell_value(rowx=i,colx=3))+"'),\n")
+        #agregamos la ultima fila con terminacion ; para finalizar comando sql
             else:
                 OuFile.writelines("('"+str(sh.cell_value(rowx=i,colx=0))+"', '"+str(Anio)+"-"+str(Mes)
                         +"-"+str(Dia)+" "+str(Horas)+":"+str(Minutos)+":"
                         +str(Segundos)+"', '"+str(sh.cell_value(rowx=i,colx=3))+"');\n")
 
+        print "hecho! ;)"
 #rescribimos el nombre del archivo agregando anio+mes+sql
         os.rename('first',(sys.argv[1].split(".")[0])+str(Anio)+"-"+str(Mes)+".sql")
     else:
